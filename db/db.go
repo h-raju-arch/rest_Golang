f3cccb7424a6d15f25c9bbd7afb2425933a8b6ef
h-raju-arch <h-raju@mercari.com>
2026-01-14 14:37:00 +0530

    removed db url

--- db/db.go
+++ db/db.go
@@ -10,7 +10,7 @@ import (
 var Db *sql.DB
 
 func InitDB() {
-	connStr := "postgresql://neondb_owner:npg_aeikcVqw2s9v@ep-dry-frost-ah8ww6ko-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+	connStr := ""
 	var err error
 	Db, err = sql.Open("postgres", connStr)
 
